14030ea2e14e08c1c5fc1e66b7badc524f3d7e44
Krish <krishshah5420@gmail.com>
2024-02-05 11:17:53 +0530

    Adding Api: Make API in first load of the component

--- table/src/Project/StaticTable.js
+++ table/src/Project/StaticTable.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import styles from "./StaticTable.module.css";
 
 const StaticTable = () => {
-  const data = [
+  const array= [
     { id: 1, name: "vcdc", age: 38, country: "India" },
     { id: 2, name: "bdeed", age: 22, country: "Canada" },
     { id: 3, name: "noxc", age: 42, country: "UK" },
+
     
   ];
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+  
+    fetch('https://jsonplaceholder.typicode.com/photos')
+      .then(response => response.json())
+      .then(data => {
+        
+        setData(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []); 
 
   return (
     <div className={ styles.container}>
@@ -22,7 +37,7 @@ const StaticTable = () => {
           </tr>
         </thead>
         <tbody>
-          {data.map((row) => (
+          {array.map((row) => (
             <tr key={row.id}>
               <td>{row.id}</td>
               <td>{row.name}</td>
